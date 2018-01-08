dcaa581f13694146afdd41a30b3e3a6a106cf60d
minyoung97 <cmy971125@naver.com>
2018-01-08 11:36:31 +0900

    민영이가 올림

--- app/src/main/java/org/techtown/hsgongmoya/MainActivity.java
+++ app/src/main/java/org/techtown/hsgongmoya/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("박정환","ㅁㄴㅇaaaaaaaaa");
+        Log.d("박정환","ㅁㄴㅇㅁㄴㅇㅁㄴㅇ");
     }
 }
